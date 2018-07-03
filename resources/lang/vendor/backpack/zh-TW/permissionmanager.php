d1176920ad6f93b752caf3a84976d7f501850985
tzcheng <mec0709@gmail.com>
2018-07-03 11:35:41 +0800

    20180703-2

--- resources/lang/vendor/backpack/zh-TW/permissionmanager.php
+++ resources/lang/vendor/backpack/zh-TW/permissionmanager.php
@@ -1,16 +1,29 @@
 <?php
 
-// --------------------------------------------------------
-// This is only a pointer file, not an actual language file
-// --------------------------------------------------------
-//
-// If you've copied this file to your /resources/lang/vendor/backpack/
-// folder, please delete it, it's no use there. You need to copy/publish the
-// actual language file, from the package.
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Permission Manager Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for Laravel Backpack - Permission Manager
+    | Author: Lúdio Oliveira <ludio.ao@gmail.com>
+    |
+     */
+    'name' => '姓名',
+    'role' => '角色',
+    'roles' => '角色',
+    'roles_have_permission' => '有此權限的角色',
+    'permission_singular' => '權限',
+    'permission_plural' => '權限',
+    'user_singular' => '使用者',
+    'user_plural' => '使用者',
+    'email' => 'Email',
+    'extra_permissions' => '額外權限',
+    'password' => 'Password',
+    'password_confirmation' => '密碼確認',
+    'user_role_permission' => '使用者的角色權限',
+    'user' => '使用者',
+    'users' => '使用者',
 
-// If a langfile with the same name exists in the package, load that one
-if (file_exists(__DIR__.'/../../../../../permissionmanager/src/resources/lang/'.basename(__DIR__).'/'.basename(__FILE__))) {
-    return include __DIR__.'/../../../../../permissionmanager/src/resources/lang/'.basename(__DIR__).'/'.basename(__FILE__);
-}
-
-return [];
+];
