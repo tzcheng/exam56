1644e7bf33cad10e9e8b411d8ab7844dd4c0a9e9
tzcheng <mec0709@gmail.com>
2018-07-03 14:27:19 +0800

    creat topic table

---
+++ app/Exam.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Exam extends Model
+{
+    //若符合慣例可以不用設定這些
+    protected $table      = 'exams';
+    protected $primaryKey = 'id';
+}
